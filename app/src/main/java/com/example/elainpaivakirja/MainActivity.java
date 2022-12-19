f123af42054d5c3cfc57fe7166629e4c0b1c0fd9
Iiro Välimaa <iiro.valimaa@edu.savonia.fi>
2022-12-20 01:25:56 +0200

    Etusivun luonti

--- app/src/main/java/com/example/elainpaivakirja/MainActivity.java
+++ app/src/main/java/com/example/elainpaivakirja/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.elainpaivakirja;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Komponentit
+        Button nappi = (Button) findViewById(R.id.nabbi);
+        TextView teksti = (TextView) findViewById(R.id.WelcomeText);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+
+
+        //  Tapahtumat
+        nappi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                teksti.setText("Moroooo");
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
     }
 }
